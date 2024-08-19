f1b41c23317e68895adbb56f63368a0940d3facc
mannuzita <manuele.oliveira@estudante.iftm.edu.br>
2024-08-18 23:55:53 -0300

    first commit

---
+++ src/main/java/br/edu/iftm/ds/integrador/sistemaveterinario/SistemaveterinarioApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.ds.integrador.sistemaveterinario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaveterinarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaveterinarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/iftm/ds/integrador/sistemaveterinario/SistemaveterinarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.ds.integrador.sistemaveterinario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaveterinarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
